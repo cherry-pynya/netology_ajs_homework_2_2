3d4cece38735b833aa36f1aae2b9e82d5d1a233b
cherry-pynya <nikita.cherepnya@gmail.com>
2021-03-05 00:13:24 +0300

    task done

---
+++ js/App/app.js
@@ -0,0 +1,13 @@
+import {Game, GameSavingData} from '../Game/game.js'
+import {readGameSaving as loadGame, writeGameSaving as saveGame} from '../Game/game.js';
+
+console.log(Game);
+console.log(GameSavingData);
+console.log(loadGame);
+console.log(saveGame);
+
+
+const game = new Game();
+game.start();
+
+
---
+++ js/Game/game.js
@@ -0,0 +1,26 @@
+import * as character from '../Domain/domain.js';
+
+
+
+export class Game {
+    start() {
+      console.log('game started');
+    }
+  }
+  
+
+
+  export class GameSavingData {
+
+  };
+  
+  export function readGameSaving() {
+
+  };
+  
+  export function writeGameSaving() {
+
+  };
+
+
+  
